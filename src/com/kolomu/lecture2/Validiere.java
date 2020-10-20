3b40a8d78e834774fc05dc8a441049756e85b167
kolomu <kevin-olomu@web.de>
2020-10-20 08:10:23 +0200

    Added Validere from Task 4 LE2

---
+++ src/com/kolomu/lecture2/Validiere.java
@@ -0,0 +1,15 @@
+package com.kolomu.lecture2;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Validiere {
+    public static void main(String[] args) {
+        while(!StdIn.isEmpty()) {
+            String inputNumber = StdIn.readLine();
+            boolean isConstancePhoneNumber = inputNumber
+                    .matches("(00|\\+|\\+\\+)49(\\s)*7531(\\s)*[1-9][0-9]{3,7}");
+            StdOut.println( isConstancePhoneNumber ? "Kostanzer Telefonnummer" : "keine Konstanzer Telefonnummer");
+        }
+    }
+}
