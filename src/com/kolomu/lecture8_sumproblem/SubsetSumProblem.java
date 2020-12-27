4174281276a4819993274faf0025d51ee0dc8d44
Kevin Olomu <kevin-olomu@web.de>
2020-12-27 18:28:38 +0100

    forgot to commit changes

--- src/com/kolomu/lecture8_sumproblem/SubsetSumProblem.java
+++ src/com/kolomu/lecture8_sumproblem/SubsetSumProblem.java
@@ -4,19 +4,35 @@ import edu.princeton.cs.algs4.StdIn;
 
 import java.util.Arrays;
 
-public class SumProblem {
+public class SubsetSumProblem {
 
     public static void main(String[] args) {
-        while (!StdIn.isEmpty()) {
-            String sIntValues = StdIn.readString();
+        System.out.println("4 Integerwerte (leerzeichen getrennt) eingeben:");
+        while(StdIn.hasNextLine()) {
             int[] intValues = Arrays
-                    .stream(sIntValues.split(" "))
+                    .stream(StdIn.readLine().split(" "))
                     .mapToInt(Integer::parseInt)
                     .toArray();
 
-            // Welche Teilmengen der Werte ergeben summiert 0
-            for(int i = 0; i < intValues.length; i++) {
-                
+            int results = 0;
+            for (int i = 0; i < intValues.length; i++) {
+                for (int j = i+1; j < intValues.length; j++) {
+                    for (int k = j+1; k < intValues.length; k++) {
+                        for (int l = k+1; l < intValues.length; l++) {
+                            if (intValues[i] + intValues[j] + intValues[k] + intValues[l] == 0) {
+                                results++;
+                                System.out.println("Found solution: " + intValues[i] + "+" + intValues[j] + "+" + intValues[k] + "+" + intValues[l]);
+                            }
+                        }
+                    }
+                }
+            }
+
+
+            if (results == 0) {
+                System.out.println("Found no result!");
+            } else {
+                System.out.println("Amount of solutions: " + results);
             }
 
         }
